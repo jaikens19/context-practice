61e34e216fd4b964294613d7fcd6f923df50f55b
Brandon Forster <brandonforster@live.com>
2021-02-11 15:34:35 -0700

    Finished Project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import coffeeBean from "./mockData/coffeeBeans.json";
+import coffeeBeans from "./mockData/coffeeBeans.json";
 import SelectedCoffeeBean from './components/SelectedCoffeeBean'
+import SetCoffeeBean from './components/SetCoffeeBean'
+import CoffeeContext from './context/CoffeeContext'
 
 
 function App() {
   return (
     <>
       <SelectedCoffeeBean/>
+      <SetCoffeeBean coffeeBeans={coffeeBeans}/>
     </>
   );
 }
--- src/components/SetCoffeeBean.js
+++ src/components/SetCoffeeBean.js
@@ -1,9 +1,16 @@
+import { useCoffee } from '../context/CoffeeContext'
+import coffeeBeans from '../mockData/coffeeBeans.json';
+
 const SetCoffeeBean = ({ coffeeBeans }) => {
+  const {coffeeBean, setCoffeeBeanId} = useCoffee()
+  console.log(coffeeBeans)
   return (
     <div className="set-coffee-bean">
       <h2>Select a Coffee Bean</h2>
       <select
         name="coffee-bean"
+        value={coffeeBean.id}
+        onChange={(val) => setCoffeeBeanId(val.target.value)}
       >
         {coffeeBeans.map(bean => (
           <option
