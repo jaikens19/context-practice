2362a9a44e42cb0ede1c47ba0026eebfca8c30f5
Brandon Forster <brandonforster@live.com>
2021-02-11 15:10:55 -0700

    Phase 1 WIP

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import coffeeBean from "./mockData/coffeeBeans.json";
+import SelectedCoffeeBean from './components/SelectedCoffeeBean'
+
 
 function App() {
   return (
     <>
-      <h1>Welcome to Coffee App</h1>
+      <SelectedCoffeeBean/>
     </>
   );
 }
